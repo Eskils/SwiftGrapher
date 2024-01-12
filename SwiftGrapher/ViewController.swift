ffcc9d7b5a1f4d426363c00e858fb6ab0e016388
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-01-12 11:08:43 +0100

    Initial Commit

---
+++ SwiftGrapher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftGrapher
+//
+//  Created by Eskil Gjerde Sviggum on 12/01/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
